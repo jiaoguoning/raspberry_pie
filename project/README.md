d3902f4bf869b3e2ce012f9a58cf69dd25aeba5f
jiaoguoning <jgn1361603343@163.com>
2020-11-28 15:47:24 +0800

    提交windows

---
+++ project/README.md
@@ -0,0 +1,16 @@
+## 树莓派安防开发分支
+
+### 开发主要用到的环境为：
+
+  - &nbsp;&nbsp;**Python >= 3.7.0 <= 3.7.9**
+
+  - &nbsp;&nbsp;**tensorflow == 1.15.0**
+  - &nbsp;&nbsp;**opencv-python == 4.4.0.46**
+  
+### 开发平台
+  - &nbsp;&nbsp;**windows10**
+  
+### 开发参考
+
+  - &nbsp;&nbsp; openpose项目： <https://github.com/CMU-Perceptual-Computing-Lab/openpose>
+  - &nbsp;&nbsp; tf-pose-estimation项目： <https://github.com/ildoonet/tf-pose-estimation>
---
+++ project/Windows/main.py
@@ -0,0 +1,151 @@
+from PyQt5 import QtCore,QtGui,QtWidgets
+import sys
+import qtawesome
+
+class MainUi(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        self.setFixedSize(960,700)
+        self.main_widget = QtWidgets.QWidget()  # 创建窗口主部件
+        self.main_layout = QtWidgets.QGridLayout()  # 创建主部件的网格布局
+        self.main_widget.setLayout(self.main_layout)  # 设置窗口主部件布局为网格布局
+
+        self.left_widget = QtWidgets.QWidget()  # 创建左侧部件
+        self.left_widget.setObjectName('left_widget')
+        self.left_layout = QtWidgets.QGridLayout()  # 创建左侧部件的网格布局层
+        self.left_widget.setLayout(self.left_layout) # 设置左侧部件布局为网格
+
+        self.right_widget = QtWidgets.QWidget() # 创建右侧部件
+        self.right_widget.setObjectName('right_widget')
+        self.right_layout = QtWidgets.QGridLayout()
+        self.right_widget.setLayout(self.right_layout) # 设置右侧部件布局为网格
+
+        self.main_layout.addWidget(self.left_widget,0,0,12,2) # 左侧部件在第0行第0列，占8行3列
+        self.main_layout.addWidget(self.right_widget,0,2,12,10) # 右侧部件在第0行第3列，占8行9列
+        self.setCentralWidget(self.main_widget) # 设置窗口主部件
+
+        self.left_close = QtWidgets.QPushButton("")  # 关闭按钮
+        self.left_visit = QtWidgets.QPushButton("")  # 空白按钮
+        self.left_mini = QtWidgets.QPushButton("")  # 最小化按钮
+
+        self.left_label_1 = QtWidgets.QPushButton("瑜伽锻炼")
+        self.left_label_1.setObjectName('left_label')
+        self.left_label_2 = QtWidgets.QPushButton("电影推荐")
+        self.left_label_2.setObjectName('left_label')
+        self.left_label_3 = QtWidgets.QPushButton("热门游戏")
+        self.left_label_3.setObjectName('left_label')
+
+        self.left_button_1 = QtWidgets.QPushButton(qtawesome.icon('fa.music', color='white'), "在线观看")
+        self.left_button_1.setObjectName('left_button')
+        self.left_button_2 = QtWidgets.QPushButton(qtawesome.icon('fa.sellsy', color='white'), "本地视频")
+        self.left_button_2.setObjectName('left_button')
+        self.left_button_3 = QtWidgets.QPushButton(qtawesome.icon('fa.film', color='white'), "热门速览")
+        self.left_button_3.setObjectName('left_button')
+        self.left_button_4 = QtWidgets.QPushButton(qtawesome.icon('fa.home', color='white'), "在线观影")
+        self.left_button_4.setObjectName('left_button')
+        self.left_button_5 = QtWidgets.QPushButton(qtawesome.icon('fa.download', color='white'), "下载管理")
+        self.left_button_5.setObjectName('left_button')
+        self.left_button_6 = QtWidgets.QPushButton(qtawesome.icon('fa.heart', color='white'), "我的收藏")
+        self.left_button_6.setObjectName('left_button')
+        self.left_button_7 = QtWidgets.QPushButton(qtawesome.icon('fa.comment', color='white'), "热门推荐")
+        self.left_button_7.setObjectName('left_button')
+        self.left_button_8 = QtWidgets.QPushButton(qtawesome.icon('fa.star', color='white'), "关注我们")
+        self.left_button_8.setObjectName('left_button')
+        self.left_button_9 = QtWidgets.QPushButton(qtawesome.icon('fa.question', color='white'), "问题反馈")
+        self.left_button_9.setObjectName('left_button')
+        self.left_xxx = QtWidgets.QPushButton(" ")
+
+        self.left_layout.addWidget(self.left_mini, 0, 0, 1, 1)
+        self.left_layout.addWidget(self.left_close, 0, 2, 1, 1)
+        self.left_layout.addWidget(self.left_visit, 0, 1, 1, 1)
+        self.left_layout.addWidget(self.left_label_1, 1, 0, 1, 3)
+        self.left_layout.addWidget(self.left_button_1, 2, 0, 1, 3)
+        self.left_layout.addWidget(self.left_button_2, 3, 0, 1, 3)
+        self.left_layout.addWidget(self.left_button_3, 4, 0, 1, 3)
+        self.left_layout.addWidget(self.left_label_2, 5, 0, 1, 3)
+        self.left_layout.addWidget(self.left_button_4, 6, 0, 1, 3)
+        self.left_layout.addWidget(self.left_button_5, 7, 0, 1, 3)
+        self.left_layout.addWidget(self.left_button_6, 8, 0, 1, 3)
+        self.left_layout.addWidget(self.left_label_3, 9, 0, 1, 3)
+        self.left_layout.addWidget(self.left_button_7, 10, 0, 1, 3)
+        self.left_layout.addWidget(self.left_button_8, 11, 0, 1, 3)
+        self.left_layout.addWidget(self.left_button_9, 12, 0, 1, 3)
+
+        self.left_close.setFixedSize(15, 15)  # 设置关闭按钮的大小
+        self.left_visit.setFixedSize(15, 15)  # 设置按钮大小
+        self.left_mini.setFixedSize(15, 15)  # 设置最小化按钮大小
+
+        self.left_close.setStyleSheet(
+            '''QPushButton{background:#F76677;border-radius:5px;}QPushButton:hover{background:red;}''')
+        self.left_visit.setStyleSheet(
+            '''QPushButton{background:#F7D674;border-radius:5px;}QPushButton:hover{background:yellow;}''')
+        self.left_mini.setStyleSheet(
+            '''QPushButton{background:#6DDF6D;border-radius:5px;}QPushButton:hover{background:green;}''')
+
+        self.left_widget.setStyleSheet('''
+            QPushButton{border:none;color:white;}
+            QPushButton#left_label{
+                border:none;
+                border-bottom:1px solid white;
+                font-size:18px;
+                font-weight:700;
+                font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+            }
+            QPushButton#left_button:hover{border-left:4px solid red;font-weight:700;}
+            QWidget#left_widget{
+                background:gray;
+                border-top:1px solid white;
+                border-bottom:1px solid white;
+                border-left:1px solid white;
+                border-top-left-radius:10px;
+                border-bottom-left-radius:10px;
+            }
+        ''')
+
+        self.right_bar_widget = QtWidgets.QWidget()  # 右侧顶部搜索框部件
+        self.right_bar_layout = QtWidgets.QGridLayout()  # 右侧顶部搜索框网格布局
+        self.right_bar_widget.setLayout(self.right_bar_layout)
+
+
+
+
+        self.right_layout.addWidget(self.right_bar_widget, 0, 0, 1, 9)
+
+
+
+        self.right_widget.setStyleSheet('''
+            QWidget#right_widget{
+                color:#232C51;
+                background:white;
+                border-top:1px solid darkGray;
+                border-bottom:1px solid darkGray;
+                border-right:1px solid darkGray;
+                border-top-right-radius:10px;
+                border-bottom-right-radius:10px;
+            }
+            QLabel#right_lable{
+                border:none;
+                font-size:16px;
+                font-weight:700;
+                font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+            }
+        ''')
+
+        self.setWindowOpacity(0.9)  # 设置窗口透明度
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)  # 设置窗口背景透明
+
+        #self.setWindowFlag(QtCore.Qt.FramelessWindowHint)  # 隐藏边框
+        self.main_layout.setSpacing(0)
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    gui = MainUi()
+    gui.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
